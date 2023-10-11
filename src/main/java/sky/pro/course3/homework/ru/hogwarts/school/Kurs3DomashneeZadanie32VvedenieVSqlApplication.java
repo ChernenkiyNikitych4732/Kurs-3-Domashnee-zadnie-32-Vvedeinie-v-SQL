ff29b1ee3be00a7b34d13fc69f445a1e26f53106
Nikita Chernenkiy <chernenkiy.nikitych136@yandex.ru>
2023-10-11 11:52:49 +0200

    Курс 3 Домашнее задание №32 Введение в SQL-изначальный комментарий

---
+++ src/main/java/sky/pro/course3/homework/ru/hogwarts/school/Kurs3DomashneeZadanie32VvedenieVSqlApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.course3.homework.ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kurs3DomashneeZadanie32VvedenieVSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kurs3DomashneeZadanie32VvedenieVSqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sky/pro/course3/homework/ru/hogwarts/school/Kurs3DomashneeZadanie32VvedenieVSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.course3.homework.ru.hogwarts.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kurs3DomashneeZadanie32VvedenieVSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
